6630f6bca522d305224d8dcf964b3308679591ca
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2025-04-16 11:57:08 -0230

    Fixed test for multiple members with same name.

--- src/pages/__tests__/GetMemberByNameSearch.test.js
+++ src/pages/__tests__/GetMemberByNameSearch.test.js
@@ -1,10 +1,10 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+   import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import GetMemberByNameSearch from "../../components/GetMemberByNameSearch";
 
 jest.mock("../../services/api");
 
 describe("GetMemberByNameSearch Component", () => {
-  test("displays member details when a member is found", () => {
+  test("displays member details when a single member is found", async () => {
     const member = {
       memberName: "John Doe",
       memberAddress: "123 Maple Street, Toronto",
@@ -35,7 +35,41 @@ describe("GetMemberByNameSearch Component", () => {
     expect(screen.getByText("2024-01-10")).toBeInTheDocument();
   });
 
-  test("displays error message when error is passed", () => {
+  test("displays list of members when multiple members are found", async () => {
+    const members = [
+      {
+        memberName: "John Doe",
+        memberAddress: "123 Maple Street, Toronto",
+        memberEmailAddress: "johndoe1@email.com",
+        memberPhoneNumber: "647-555-1234",
+        memberStartDate: "2024-01-10",
+        duration: "1 year",
+      },
+      {
+        memberName: "John Doe",
+        memberAddress: "213 Wing Street, Toronto",
+        memberEmailAddress: "doe1@email.com",
+        memberPhoneNumber: "647-555-5678",
+        memberStartDate: "2024-02-20",
+        duration: "2 years",
+      },
+    ];
+
+    render(
+      <GetMemberByNameSearch
+        member={null}
+        members={members}
+        error={null}
+        onBack={jest.fn()}
+      />
+    );
+
+    expect(screen.getByText("Members Found")).toBeInTheDocument();
+    const johnDoes = screen.getAllByText("John Doe");
+    expect(johnDoes).toHaveLength(2);
+  });
+
+  test("displays error message when error is passed", async () => {
     const errorMessage = "No members found for this name";
 
     render(
@@ -51,20 +85,5 @@ describe("GetMemberByNameSearch Component", () => {
       screen.getByText("No members found for this name")
     ).toBeInTheDocument();
   });
-
-  test("calls onBack function when 'Go Back' button is clicked", () => {
-    const onBackMock = jest.fn();
-
-    render(
-      <GetMemberByNameSearch
-        member={null}
-        members={[]}
-        error={null}
-        onBack={onBackMock}
-      />
-    );
-
-    fireEvent.click(screen.getByText("Go Back"));
-    expect(onBackMock).toHaveBeenCalledTimes(1);
-  });
 });
+
